f8955fd1d3080a2c500bfcc803fd5c2f0aca6974
hos8 <jam39so@outlook.com>
2024-06-06 12:44:40 +0800

    Modify certainly.c

--- certainly.c
+++ certainly.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Always rather run simply time couple all go.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
